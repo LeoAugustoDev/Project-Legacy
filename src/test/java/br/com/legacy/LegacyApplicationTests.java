a740226f6ef53234901003542ffb28a884854100
LeoAugustoDev <leoaugustoalves58@gmail.com>
2026-06-10 14:58:53 -0300

    first commit

---
+++ src/test/java/br/com/legacy/LegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.legacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
